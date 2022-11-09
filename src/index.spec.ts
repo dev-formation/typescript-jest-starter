f4c16fa657296661b773e9f9f8bdc7282baaadde
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-11-09 16:18:49 +0100

    install jest + add config + add script test

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+describe('Un ensemble de test', () => {
+  test('Test exemple1', () => {
+    const actual = 1;
+    expect(actual).toBe(1);
+  });
+
+  test('Test exemple2', () => {
+    const actual = 0;
+    expect(actual).toBe(1);
+  });
+});
